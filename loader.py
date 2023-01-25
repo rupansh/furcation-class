372ca004e349d5cc3311dc578143c280077af3b2
rupansh <rupanshsekar@hotmail.com>
2023-01-25 21:26:13 +0530

    update for new dataset

--- loader.py
+++ loader.py
@@ -2,55 +2,41 @@ from glob import glob
 import cv2
 import numpy as np
 from util import *
+import json
 
 resize_sz = (320, 320)
 
-def rotate_bb_90(bndbox, original_w, original_h):
-    l, t, r, b = yolo_voc(bndbox, (original_w, original_h))
-    nl = t
-    nt = original_w - r
-    nr = b
-    nb = original_w - l
- 
-    return voc_yolo([nl, nt, nr, nb], (original_h, original_w))
-
 def load_data(base_path, n = None, rs = resize_sz):
+    with open(f"{base_path}/labels.json") as labf:
+        labs = json.load(labf)
+
     x = []
     y = []
     im_cnt = 0
-    for img in glob(f"{base_path}/images/*.jpg"):
-        im = cv2.imread(img)
-
-        h, w, _ = im.shape
-        rot = False
-        if w > h:
-            rot = True
-            im = cv2.rotate(im, cv2.ROTATE_90_COUNTERCLOCKWISE)
-
+    for lab in labs:
+        impath = lab["file_upload"]
+        im = cv2.imread(f"{base_path}/{impath}")
         if rs:
             im = cv2.resize(im, rs)
-
         im = np.asarray(im).astype(np.float32)
+        h, w, _ = im.shape
         x.append(im)
-
-        label_name = img.split("/").pop()[:-4]
-        with open(f"{base_path}/labels/{label_name}.txt", "r") as lab_f:
-            lab_s = lab_f.readline().strip("\n")
-            lab = [float(cord) for cord in lab_s.split()[1:]]
-
-            if rot:
-                lab = rotate_bb_90(lab, w, h)
-            
-            y.append(np.array(lab).astype(np.float32))
+        truth = lab["annotations"][0]["result"][0]["value"]
+        #y.append(np.asarray([box["x"]/w, box["y"]/h, box["width"]/w, box["height"]/h]))
+        categ = truth["rectanglelabels"][0][6:]
+        grade = int(categ) if categ.isnumeric() else 0
+        out = [1 if i == grade else 0 for i in range(1, 4)]
+        y.append(np.asarray(out).astype(np.float32))
 
         im_cnt += 1
         if n and im_cnt == n:
             break 
     
     return np.array(x), np.array(y)
+    
 
 def load_train_data(n = None, rs = resize_sz):
-    return load_data("./data/train", n, rs)
+    return load_data("./data/train/data-full", n, rs)
 
 def load_test_data(n = None, rs = resize_sz):
     return load_data("./data/test", n, rs)
--- train.py
+++ train.py
@@ -10,7 +10,7 @@ def preprocess(x, y):
     return xp, y
 
 print("loading training data")
-X, Y = load_train_data()
+X, Y = load_train_data(1000)
 X, Y = preprocess(X, Y) 
 print("loaded training data!")
 
@@ -19,11 +19,11 @@ input_tensor = keras.Input(shape=X.shape[1:])
 resnet = keras.applications.ResNet50(include_top=False, weights="imagenet", input_tensor=input_tensor)
 
 # Freeze all blocks except last
-#for l in resnet[:143]:
-resnet.trainable = False
+for l in resnet.layers[:143]:
+    l.trainable = False
 
 model = keras.models.Sequential()
-#model.add(keras.layers.Lambda(lambda im: tf.image.resize(im, (256, 256))))
+#model.add(keras.layers.Lambda(lambda im: tf.image.resize(im, (224, 224))))
 model.add(resnet)
 model.add(keras.layers.Flatten())
 model.add(keras.layers.BatchNormalization())
@@ -36,10 +36,12 @@ model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dropout(0.5))
 model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Dense(4, activation='sigmoid'))
+#model.add(keras.layers.Dense(4, activation='sigmoid'))
+model.add(keras.layers.Dense(3, activation="softmax"))
 
-model.compile(loss='mse',
-                optimizer=keras.optimizers.Adam(learning_rate=0.05),
+model.compile(#loss="mse" ,
+                loss="categorical_crossentropy",
+                optimizer=keras.optimizers.Adam(learning_rate=0.01),
                 metrics=['accuracy'])
 
 history = model.fit(X, Y, batch_size=4, epochs=5, verbose=1)
